017b951f2b0d243dd9d51666d23302c26e24d7dd
lewis <ljones140@gmail.com>
2015-08-03 12:20:14 +0100

    added shotgun and kitten

--- hello.rb
+++ hello.rb
@@ -3,3 +3,10 @@ require 'sinatra'
 get '/' do
   'hello'
 end
+
+get '/secret' do
+  'this is a secret page that changes, another changes'
+  "<div>
+  <img src='http://bit.ly/1eze8aE'alt='scary cat'>
+  </div>"
+end
