9cd387ce8d5195d91857589fdaaf4460b42a6a97
Makers Laptop 22 <makerslaptop22@Makerss-MacBook-Pro-6.local>
2015-08-03 13:09:58 +0100

    Added index .erb file

--- hello.rb
+++ hello.rb
@@ -5,8 +5,5 @@ get '/' do
 end
 
 get '/secret' do
-  'this is a secret page that changes, another changes'
-  "<div>
-  <img src='http://bit.ly/1eze8aE'alt='scary cat'>
-  </div>"
+  erb :index
 end
